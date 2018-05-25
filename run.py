cc2d6d0064d6d8da1c2bd4d0d80f90e3afbe681d
Chan Tai To <33389684+BigtoC@users.noreply.github.com>
2018-05-25 22:36:27 +0800

    Update run.py

--- run.py
+++ run.py
@@ -3,42 +3,40 @@ import time
 
 
 urls = ['http://cn-proxy.com/',
-        'http://free-proxy.cz/zh/proxylist/country/CN/all/speed/level3/1',
-        # 'https://www.kuaidaili.com/free/intr/'  # 快代理的ip虽然快，但是在网易云测试过都不能用，this website sucks
+        'http://free-proxy.cz/zh/proxylist/country/CN/all/ping/all',
+        #  'https://www.kuaidaili.com/free/intr/',  # 快代理的ip虽然快，但是在网易云测试过都不能用，this website sucks
+        'http://www.xicidaili.com/nt/'
         ]
-good_ip = []
 
 
-def compare_ips():
-    good_ip.sort(key=lambda x: (x["speed"]), reverse=True)
-
-
-def print_result():
-    compare_ips()
-    if good_ip[0]["ip"] and good_ip[0]["speed"] and good_ip[0]["port"] != -1:
-        print('The best ip address is: \n{}'.format(good_ip[0]["ip"]))
+def print_result(selected):
+    print()
+    if selected[0].ip and selected[0].port and selected[0].speed != -1:
+        print('The best ip address is: \n{}'.format(selected[0].ip))
         time.sleep(0.5)
-        print('Its port number is: \n{}'.format(good_ip[0]["port"]))
+        print('Its port number is: \n{}'.format(selected[0].port))
         time.sleep(0.5)
-        print('Its response time is: \n{}ms'.format(good_ip[0]["speed"]))
+        print('Its response time is: \n{}ms'.format(selected[0].speed))
         time.sleep(0.5)
-        print('(*^o^*)')
+        print('This is for backup: {}:{}'.format(selected[1].ip, selected[1].port))
+
     else:
         print("Error T_T")
 
 
 if __name__ == '__main__':
 
-    for url in urls:
-        # start = time.clock()
-        analyzeIP.analyze_ip(url)
-        selected_ip = analyzeIP.return_ip()
-        good_ip.append(selected_ip)
-        # end = time.clock()
-        # print(end - start)
+    start = time.clock()
 
-    print_result()
+    analyzeIP.thread_get_ip(urls)  # First get all IPs and ports
 
-    print()
+    analyzeIP.thread_analyze()  # Then multi thread ping
 
+    selected_ip = analyzeIP.return_ip()  # Get the selected IP's info
 
+    end = time.clock()
+
+    print_result(selected_ip)
+    print('It takes {} seconds'.format(int(end - start)))
+    print('(*^o^*)')
+    input()
