e92c116ededf07995f9c0ea9263dad06dc3e349b
Chan Tai To <33389684+BigtoC@users.noreply.github.com>
2018-05-25 22:37:15 +0800

    Update getIP.py

--- getIP.py
+++ getIP.py
@@ -5,22 +5,28 @@ from selenium.webdriver.chrome.options import Options
 import platform
 
 
+class Tmp:
+    def __init__(self):
+        self.ip = -1
+        self.port = -1
+
+
 ip_regex = re.compile(r'\d+\.\d+\.\d+\.\d+')
+
+headers = {
+    'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36',
+    'Cookie': '_ga=GA1.2.1328575144.1520154766; _gid=GA1.2.1755846933.1526619066',
+    'Connection': 'keep-alive',
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, deflate, sdch, base64',
+    'Accept-Language': 'zh-CN,zh;q=0.8',
+    }
 
 
 def get_html(url):
-    print('Requesting {}'.format(url))
-    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36'
-    headers = {
-        'User-agent': user_agent,
-        'Cookie': '_ga=GA1.2.1328575144.1520154766; _gid=GA1.2.1755846933.1526619066',
-        'Connection': 'keep-alive',
-        'Accept': '*/*',
-        'Accept-Encoding': 'gzip, deflate, sdch',
-        'Accept-Language': 'zh-CN,zh;q=0.8',
-    }
 
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.set_headless()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     driver = None
@@ -46,20 +52,20 @@ def dispatch(url):
         dic = f_get_ip(url)
     if 'kuaidaili' in url:
         dic = k_get_ip(url)
+    if 'xicidaili' in url:
+        dic = x_get_ip(url)
 
     # dic['118.114.77.47'] = 8080
     # dic['39.134.68.23'] = 80
-
     return dic
 
 
 def c_get_ip(url):
     html = get_html(url)
 
-    ip_port = {}
+    ip_port = []
 
     port_regex = re.compile(r'<td>\n             \d+\n            </td>')
-    print('Getting IP addresses from cn-proxy.com...')
 
     all_ip = re.findall(ip_regex, html)
     tmp_port = re.findall(port_regex, html)
@@ -70,16 +76,18 @@ def c_get_ip(url):
         all_port.append(p)
 
     for i in range(len(all_ip)):
-        ip_port[all_ip[i]] = all_port[i]
+        t = Tmp()
+        t.ip = all_ip[i]
+        t.port = all_port[i]
+        ip_port.append(t)
 
+    print('Got {} IPs from cn-proxy.com'.format(len(ip_port)))
     return ip_port
 
 
 def f_get_ip(url):
     html = get_html(url)
-    ip_port = {}
-
-    print('Getting IP addresses from free-proxy.cz...')
+    ip_port = []
 
     port_regex = re.compile(r'<span class="fport" style="">\n         \d+\n        <\/span>')
     tmp_ip = re.findall(re.compile(r'</script>\n        \d+\.\d+\.\d+\.\d+\n       <\/td>|'
@@ -98,19 +106,21 @@ def f_get_ip(url):
         all_port.append(p)
 
     for i in range(len(all_ip)):
-        ip_port[all_ip[i][0]] = all_port[i]
+        t = Tmp()
+        t.ip = all_ip[i][0]
+        t.port = all_port[i]
+        ip_port.append(t)
 
+    print('Got {} IPs from free-proxy.cz'.format(len(ip_port)))
     return ip_port
 
 
 def k_get_ip(url):  # Abandoned! Ignore this!
-    ip_port = {}
-    page_num = 3  # change the number if you want get more ip addresses
+    ip_port = []
+    page_num = 2  # change the number if you want get more ip addresses
     for i in range(page_num):
         url = 'https://www.kuaidaili.com/free/intr/{}/'.format(i+1)
         html = get_html(url)
-
-        print('Getting IP addresses from kuaidaili.com...')
 
         port_regex = re.compile(r'<td data-title="PORT">\n           \d+\n          </td>')
         all_ip = re.findall(ip_regex, html)
@@ -122,21 +132,58 @@ def k_get_ip(url):  # Abandoned! Ignore this!
             p = re.sub(r'\D', '', p)
             all_port.append(p)
 
-        for i in range(len(all_ip)):
-            ip_port[all_ip[i]] = all_port[i]
+        for j in range(len(all_ip)):
+            t = Tmp()
+            t.ip = all_ip[j]
+            t.port = all_port[j]
+            ip_port.append(t)
 
+    print('Got {} IPs from kuaidaili.com'.format(len(ip_port)))
+    return ip_port
+
+
+def x_get_ip(url):
+    ip_port = []
+    page_num = 2
+    port_regex = re.compile(r'       <td>\n        \d+\n       </td>')
+
+    for i in range(page_num):
+        u = url + str(i+1)
+        html = get_html(u)
+
+        all_ip = re.findall(ip_regex, html)
+        tmp_port = re.findall(port_regex, html)
+
+        all_port = []
+        for p in tmp_port:
+            p = re.sub(r'\D', '', p)
+            all_port.append(p)
+
+        for j in range(len(all_ip)):
+            t = Tmp()
+            t.ip = all_ip[j]
+            t.port = all_port[j]
+            ip_port.append(t)
+
+    print('Got {} IPs from xicidaili.com'.format(len(ip_port)))
     return ip_port
 
 
 # if __name__ == '__main__':
-#     url = 'http://free-proxy.cz/zh/proxylist/country/CN/all/speed/level3'
-#     html = get_html(url)
-#     file = open("2.html", "w", encoding='utf-8')
-#     file.write(html)
-#     file.close()
-#     print(html)
-#
-#     ip_port = f_get_ip(url)
-#     print(ip_port)
-#     print(len(ip_port))
+#     url = 'http://www.xicidaili.com/nn/'
+#     d = dispatch(url)
+#     print(len(d))
+#     print(d)
+
+    # html = get_html(url)
+    # file = open("2.html", "w", encoding='utf-8')
+    # file.write(html)
+    # file.close()
+    # print(html)
+    #
+    # ip_port = f_get_ip(url)
+    # print(ip_port)
+    # print(len(ip_port))
+    # r = requests.get('https://1.1.1.1', proxies={"http": "http://222.33.192.238:8118"})
+    # print(r.status_code)
 
